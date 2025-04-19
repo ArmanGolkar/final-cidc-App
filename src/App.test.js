daa26f5241bc1a3d2963ab6659c86ae8ad0100b7
golkar <arman_h0@yahoo.com>
2025-04-18 19:15:05 -0600

    group name update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders group number and team members', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Group 7/i)).toBeInTheDocument();
+  expect(screen.getByText(/Arman/i)).toBeInTheDocument();
 });
